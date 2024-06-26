b9b51f7fee29e181af73cac2cfccc7bd13b648fc
mizray <paraxanoe@gmail.com>
2024-06-25 21:20:48 -0300

    Add files via upload

---
+++ exercicio.py
@@ -0,0 +1,7 @@
+print("Programa de Repetição até digitar 0")
+
+user = int(input("Digite o número Inteiro"))
+while user != 0:
+    print("Diferente de zero!!!!")
+    user = int(input("Digite um número inteiro"))
+print("Fim do programa!!")
